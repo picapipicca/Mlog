05118dcae1c75099bbe0fb8e2d8bfee6cbf215bd
picapipicca <haewon09106@gmail.com>
2022-03-01 16:43:20 +0900

    addComment complete (firestore,redux)
    addComment complete (firestore,redux)

--- src/element/Input.js
+++ src/element/Input.js
@@ -1,68 +1,101 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "styled-components";
 
 const Input = (props) => {
-  const { label, type, placeholder,multiLine, _onChange, length, bottomLined,value} = props;
-  const styles = { length };
+  const {
+    width,
+    label,
+    type,
+    placeholder,
+    multiLine,
+    _onChange,
+    length,
+    bottomLined,
+    value,
+    is_submit,
+  } = props;
+  const styles = { length, width };
 
   const classes = props.className;
 
   if (multiLine) {
     return (
-        <TextareaTag
-          rows = {2}
-          placeholder={placeholder}
-          onChange={_onChange}
-        ></TextareaTag>
+      <Fragment>
+        {is_submit ? (
+          <TextareaTag
+            value={value}
+            rows={2}
+            placeholder={placeholder}
+            onChange={_onChange}
+          ></TextareaTag>
+        ) : (
+          <TextareaTag
+            rows={2}
+            placeholder={placeholder}
+            onChange={_onChange}
+          ></TextareaTag>
+        )}
+      </Fragment>
     );
   }
-
 
   if (bottomLined) {
     return (
       <div className={classes}>
         {label && <p margin="0px">{label}</p>}
-        <Underlined
+      {is_submit?   <Underlined
           {...styles}
           type={type}
-          // value={value}
+          value={value}
           placeholder={placeholder}
           onChange={_onChange}
-        />
+        />:  <Underlined
+        {...styles}
+        type={type}
+        placeholder={placeholder}
+        onChange={_onChange}
+      />}
       </div>
     );
   }
   return (
     <div className={classes}>
       {label && <p margin="0px">{label}</p>}
-      <InputTag
+     {is_submit?  <InputTag
         {...styles}
         type={type}
         value={value}
         placeholder={placeholder}
         onChange={_onChange}
-      />
+      />:  <InputTag
+      {...styles}
+      type={type}
+      placeholder={placeholder}
+      onChange={_onChange}
+    /> }
     </div>
   );
 };
 
 Input.defaultProps = {
+  multiLine: false,
+  bottomLined: false,
   label: false,
   placeholder: "텍스트를 입력해주세요.",
   type: "text",
   value: "",
+  // className:false,
+  is_submit:false,
   _onChange: () => {},
-  width: '100%',
 };
 
 const InputTag = styled.input`
-  /* ${(props) => (props.className ? `className: ${props.className};` : "")} */
-
+  ${(props) => (props.className ? `className: ${props.className};` : "")}
   /* ${(props) => (props.length ? `maxLength: ${props.length};` : "")} */
   margin: 1rem auto;
   padding: 1rem;
-  width: ${(props)=> props.width};
-  max-width: 40rem;
+  width: ${(props) => props.width};
+  /* max-width: 40rem; */
   border: 1px solid #212121;
   box-sizing: border-box;
   display: block;
@@ -83,7 +116,7 @@ width: 90vw;
 `;
 const TextareaTag = styled.textarea`
   border: 1px solid #212121;
-  width:${(props)=> props.width};
+  width: ${(props) => props.width};
   padding: 12px 4px;
   box-sizing: border-box;
 `;
