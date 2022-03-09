5d9b4df792279a325950ed13edf3482d9630ee4f
picapipicca <haewon09106@gmail.com>
2022-03-10 01:21:54 +0900

    Post component updated
    React.memo() 사용하여 post component rerendering 방지

--- src/components/Log/Post.js
+++ src/components/Log/Post.js
@@ -1,45 +1,46 @@
-import React, { Fragment } from "react";
+import React from "react";
 import classes from "./Post.module.css";
 import { Grid, Button, Image } from "../../element/index";
 import { history } from "../../redux/configStore";
 import { Viewer } from "@toast-ui/react-editor";
 
-const Post = (props) => {
-  return (
-    <div className={classes.wrap}>
-      <div className={classes["top-header"]}>
-        <Image circle src={props.user_info.user_profile} />
-        <p>{props.user_info.user_nick}</p>
-        {props.its_me && (
-          <button
-            className={classes["btn-edit"]}
-            onClick={() => {
-              history.push(`/write/${props.id}`);}}>
-                수정</button>)}
-      </div>
-
-      <div className={classes.header}>
-        <div className={classes["header-is-closed"]}>
-          <div> 로그기록 : {props.title}</div>
+const Post = React.memo((props) => {
+    
+    return (
+      <div className={classes.wrap}>
+        <div className={classes["top-header"]}>
+          <Image circle src={props.user_info.user_profile} />
+          <p>{props.user_info.user_nick}</p>
+          {props.its_me && (
+            <button
+              className={classes["btn-edit"]}
+              onClick={() => {
+                history.push(`/write/${props.id}`);}}>
+                  수정</button>)}
+        </div>
+  
+        <div className={classes.header}>
+          <div className={classes["header-is-closed"]}>
+            <div> 로그기록 : {props.title}</div>
+          </div>
+        </div>
+  
+        <div className={classes.body}>
+          <div className={classes.viewer}>
+            <Viewer initialValue={props.content} height="auto" />
+          </div>
+  
+          <div className={classes["body-footer"]}>
+            <hr style={{ opacity: "0.8", borderColor: "#EF5A31" }} />
+            <Grid is_flex width="auto">
+              <p style={{ margin: "0px 10px 0px 0px" }}>{props.insert_dt}</p>
+              <p style={{ margin: "0px" }}>댓글 {props.comment_count}개</p>
+            </Grid>
+          </div>
         </div>
       </div>
-
-      <div className={classes.body}>
-        <div className={classes.viewer}>
-          <Viewer initialValue={props.content} height="auto" />
-        </div>
-
-        <div className={classes["body-footer"]}>
-          <hr style={{ opacity: "0.8", borderColor: "#EF5A31" }} />
-          <Grid is_flex width="auto">
-            <p style={{ margin: "0px 10px 0px 0px" }}>{props.insert_dt}</p>
-            <p style={{ margin: "0px" }}>댓글 {props.comment_count}개</p>
-          </Grid>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  });
 Post.defaultProps = {
   user_info: {
     user_email: "sexy@sexy.com",
